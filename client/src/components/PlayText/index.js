756a6ddb8744c0f03d152732b9076a8672ca1f68
Chance <chancecrawford30@gmail.com>
2022-06-10 19:00:04 -0700

    cleaned up playText function in PlayText component

--- client/src/components/PlayText/index.js
+++ client/src/components/PlayText/index.js
@@ -11,68 +11,7 @@ function PlayText({ chapter }) {
     const [playing, setPlaying] = useState('None');
     const [synthVoices, setSynthVoices] = useState({});
 
-    // on google chrome speech synthesis will usually cut out after about 
-    // 200 characters in an utterance. This is an open source well known
-    // workaround to break the uterance string into smaller utterances and play
-    // them one after another. The utterances are split at punctuation.
-    var speechUtteranceChunker = function (utt, settings, callback) {
-        settings = settings || {};
-        var newUtt;
-        var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
-        if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
-            newUtt = utt;
-            newUtt.text = txt;
-            newUtt.addEventListener('end', function () {
-                if (speechUtteranceChunker.cancel) {
-                    speechUtteranceChunker.cancel = false;
-                }
-                if (callback !== undefined) {
-                    callback();
-                }
-            });
-        }
-        else {
-            var chunkLength = (settings && settings.chunkLength) || 160;
-            var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
-            var chunkArr = txt.match(pattRegex);
-    
-            if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
-                //call once all text has been spoken...
-                if (callback !== undefined) {
-                    callback();
-                }
-                return;
-            }
-            var chunk = chunkArr[0];
-            newUtt = new SpeechSynthesisUtterance(chunk);
-            setCurrentUtt(newUtt);
-            var x;
-            for (x in utt) {
-                if (utt.hasOwnProperty(x) && x !== 'text') {
-                    newUtt[x] = utt[x];
-                }
-            }
-            newUtt.addEventListener('end', function () {
-                if (speechUtteranceChunker.cancel) {
-                    speechUtteranceChunker.cancel = false;
-                    return;
-                }
-                settings.offset = settings.offset || 0;
-                settings.offset += chunk.length - 1;
-                speechUtteranceChunker(utt, settings, callback);
-            });
-        }
-    
-        if (settings.modifier) {
-            settings.modifier(newUtt);
-        }
-        console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
-        //placing the speak invocation inside a callback fixes ordering and onend issues.
-        setTimeout(function () {
-            speechSynthesis.speak(newUtt);
-        }, 0);
-    };
-
+    // on load of component, do this.
     useEffect(() => {
         setSpeech()
         .then((voices)=>{ 
@@ -113,23 +52,12 @@ function PlayText({ chapter }) {
 
     async function setUtter(i, arr) {
         return new Promise(resolve => {
-                    let utter = new SpeechSynthesisUtterance(arr[i]);
-                    utter.voice = synthVoices[0];
-                    setCurrentUtt(utter);
-                    synth.speak(utter);
-                    utter.onend = resolve;
-
-                
-                
-    
-                // id = setInterval(() => {
-                //     if (utter) {
-                //         resolve(synth.speak(utter));
-                //         clearInterval(id);
-                //     }
-                // }, 50);
+            let utter = new SpeechSynthesisUtterance(arr[i]);
+            utter.voice = synthVoices[0];
+            setCurrentUtt(utter);
+            synth.speak(utter);
+            utter.onend = resolve;
         });
-
     }
 
     function chunkString(str, offset) {
